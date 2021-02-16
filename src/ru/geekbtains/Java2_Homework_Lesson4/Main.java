74510f6e30f86d11c36d7c0f24b71d32dbf58216
Ildar <Sayidov.ildar@gmail.com>
2021-02-16 19:38:57 +0300

    need more time

--- src/ru/geekbtains/Java2_Homework_Lesson4/Main.java
+++ src/ru/geekbtains/Java2_Homework_Lesson4/Main.java
@@ -3,6 +3,6 @@ package ru.geekbtains.Java2_Homework_Lesson4;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("need more time");
     }
 }
